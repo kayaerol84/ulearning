3b7c4874f2d74dc675b2fd7f374ebfd6bd3fa3f2
Erol Kaya <kayaerol84@gmail.com>
2016-05-12 00:10:05 +0200

    fix model issues

--- src/main/java/com/ulearning/model/Learner.java
+++ src/main/java/com/ulearning/model/Learner.java
@@ -7,15 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name="Learner")
 @PrimaryKeyJoinColumn(name="learner_id", referencedColumnName="id")
 public class Learner extends User{
 	
-	// learner average score
 	@Column(precision=3, scale=5) 
-	private Float avarageScore;
+	private Float avarageScore; 
 	
 	@OneToMany
 	private List<Training> subscribedTrainings;
